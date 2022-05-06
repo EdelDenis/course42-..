702a01c35125b5372100d1e0033f7791b40a8c6e
EdelDenis <edeldenis12@gmail.com>
2022-05-06 12:13:36 +0000

    j

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 "use strict"
 
-const box = document.getElementById("box");
-const btns = document.getElementsByTagName("button");
-const circles = document.getElementsByClassName("circle");
-const hearts = document.querySelectorAll(".heart");
-const oneHeart = document.querySelector(".heart");
+const box = document.getElementById("box"); // - найти єлемент по ид 
+const btns = document.getElementsByTagName("button"); // - найти єлемент по хтмл тегу
+const circles = document.getElementsByClassName("circle"); // - найти єлемент по классу (без точки)
+const hearts = document.querySelectorAll(".heart"); // - найти єлемент по селектору (нужна точка или ид) - можно еще с индексами из псевдомассива
+const oneHeart = document.querySelector(".heart"); // - найти єлемент по селектору (всегда віводится первій)
 const wrapper = document.querySelector(".wrapper");
 
 //box.style.backgroundColor = "blue";
@@ -23,18 +23,27 @@ hearts.forEach(item =>{
     item.style.backgroundColor = "blue";
 }); /* переборка псевдомассива */
 
-const div = document.createElement("div"); // - создали переменную с дивом 
+const div = document.createElement("div"); // - создали переменную с каким то дивом 
 //const text = document.createTextNode("Тут был я");
 
 div.classList.add("black"); // - добавили к диву класс из цсс селекторов
 
-// wrapper.append(div); // - добавить К враперу див в КОНЕЦ
+//wrapper.append(div); // - добавить К враперу(класс с сердечками) див в КОНЕЦ (посл во врапере)
 
-// wrapper.prepend(div); // - добавить К враперу див в НАЧАЛО 
+// wrapper.prepend(div); // - добавить К враперу див в НАЧАЛО (перед врапером)
 
 // hearts[0].before(div) // - добавить див ПЕРЕД выбраным элементом (хартс 0);
 
 // hearts[0].after(div) // - добавить див ПОСЛЕ выбранного элемента (хартс 0);
+
+// circles[0].remove(); // - удалить єлемент с индексом со страниці
+
+// hearts[0].replaceWith(circles[0]); // - заменить элемент другим элементом (с индексами)
+
+// ----------------- устаревшие конструкции ------------------------
+
+// wrapper.appendChild(div);
+
 
 
 
